cd89cfe8b8423ec843c7dd168d5136cc5af6a262
matin ghaedi <ghaedidev@gmail.com>
2022-06-23 10:30:55 +0430

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  position: absolute;
+  width: 1440px;
+  height: 48px;
+  left: 0px;
+  top: 0px;
+
+  background-color: #11b980;
+}
+</style>
